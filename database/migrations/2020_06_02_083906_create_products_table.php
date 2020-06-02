a05ecd9ff42deffaab83f7e2c684482cdf504a65
Nicolas Durocher <ndurocher@hotmail.fr>
2020-06-02 11:05:46 +0200

    Adding Tables Categories and Products in DB

---
+++ database/migrations/2020_06_02_083906_create_products_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title', 100); // VARCHAR 100
+            $table->text('description'); // TEXT NULL
+            $table->decimal('price', 8, 2);	
+            $table->enum('size', ['small', 'medium', 'large', 'extra-large']);
+            $table->string('url_image'); 
+            $table->enum('status', ['published', 'unpublished'])->default('unpublished');
+            $table->enum('code', ['solde', 'new']);
+            $table->string('reference', 200); // VARCHAR 200
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
