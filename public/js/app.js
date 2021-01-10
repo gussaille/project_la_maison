6d1920d178e8ac965b785fb2dc7917ccbc482857
Nico D <ndurocher@hotmail.fr>
2021-01-10 13:31:48 +0100

    Editing procfile

--- public/js/app.js
+++ public/js/app.js
@@ -37292,8 +37292,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\wamp64\www\Projet_La_Maison\La_Maison\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\wamp64\www\Projet_La_Maison\La_Maison\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\nduro\OneDrive\Documents\projets_perso\Projet_La_Maison\La_Maison\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\nduro\OneDrive\Documents\projets_perso\Projet_La_Maison\La_Maison\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
